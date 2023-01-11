e38e39c582c5a972d00ad1e47700e12a8d898974
Eyüp Can Dündar <eyup@ecdyazilim.com>
2023-01-11 10:32:13 +0300

    json accept

--- lib/services/LoginService.dart
+++ lib/services/LoginService.dart
@@ -1,5 +1,20 @@
+import 'dart:async';
+import 'package:http/http.dart' as http;
+
 class LoginService {
   static LoginService? _instance;
   factory LoginService() => _instance ??= new LoginService._();
   LoginService._();
+
+  Future girisYap(String kullaniciAdi, String kullaniciSifre) async {
+    Completer c = new Completer();
+    try {
+      String url = "https://petekapi.burulas.com.tr/users/authenticate";
+      http.post(Uri.parse(url), headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      });
+    } catch (e) {}
+    return c.future;
+  }
 }
