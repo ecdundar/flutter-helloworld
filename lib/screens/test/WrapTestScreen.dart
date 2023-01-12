64cf2a391e53b007002a2118d145b858da49e5bf
Eyüp Can Dündar <eyup@ecdyazilim.com>
2023-01-12 11:46:57 +0300

    WarpScreen

--- lib/screens/test/WrapTestScreen.dart
+++ lib/screens/test/WrapTestScreen.dart
@@ -13,84 +13,112 @@ class _WrapTestScreenState extends State<WrapTestScreen> {
     return SafeArea(
         child: Container(
       color: Colors.yellow,
-      child: Wrap(children: [
-        Chip(
-          label: Text("Deneme 1"),
-          avatar: CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
-        ),
-        Chip(
-          label: Text("Deneme 1"),
-          avatar: CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
-        ),
-        Chip(
-          label: Text("Deneme 1"),
-          avatar: CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
-        ),
-        Chip(
-          label: Text("Deneme 1"),
-          avatar: CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
-        ),
-        Chip(
-          label: Text("Deneme 1"),
-          avatar: CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
-        ),
-        Chip(
-          label: Text("Deneme 1"),
-          avatar: CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
-        ),
-        Chip(
-          label: Text("Deneme 1"),
-          avatar: CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
-        ),
-        Chip(
-          label: Text("Deneme 1"),
-          avatar: CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
-        ),
-        Chip(
-          label: Text("Deneme 1"),
-          avatar: CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
-        ),
-        Chip(
-          label: Text("Deneme 1"),
-          avatar: CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
-        ),
-        Chip(
-          label: Text("Deneme 1"),
-          avatar: CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
-        ),
-        Chip(
-          label: Text("Deneme 1"),
-          avatar: CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
-        ),
-        Chip(
-          label: Text("Deneme 1"),
-          avatar: CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
-        ),
-        Chip(
-          label: Text("Deneme 1"),
-          avatar: CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
-        ),
-        Chip(
-          label: Text("Deneme 1"),
-          avatar: CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
-        ),
-        Chip(
-          label: Text("Deneme 1"),
-          avatar: CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
-        ),
-        Chip(
-          label: Text("Deneme 1"),
-          avatar: CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
-        ),
-        Chip(
-          label: Text("Deneme 1"),
-          avatar: CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
-        ),
-        Chip(
-          label: Text("Deneme 1"),
-          avatar: CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
-        ),
-      ]),
+      child: Wrap(
+          alignment: WrapAlignment.start,
+          crossAxisAlignment: WrapCrossAlignment.start,
+          direction: Axis.vertical,
+          spacing: 20,
+          runSpacing: 150,
+          children: [
+            Chip(
+              onDeleted: () {
+                print("deneme");
+              },
+              label: Text("Deneme 1"),
+              avatar:
+                  CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
+            ),
+            Chip(
+              label: Text("Deneme 1"),
+              avatar:
+                  CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
+            ),
+            Chip(
+              label: Text("Deneme 1"),
+              avatar:
+                  CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
+            ),
+            Chip(
+              label: Text("Deneme 1"),
+              avatar:
+                  CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
+            ),
+            Chip(
+              label: Text("Deneme 1"),
+              avatar:
+                  CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
+            ),
+            Chip(
+              label: Text("Deneme 1"),
+              avatar:
+                  CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
+            ),
+            Chip(
+              label: Text("Deneme 1"),
+              avatar:
+                  CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
+            ),
+            Chip(
+              label: Text("Deneme 1"),
+              avatar:
+                  CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
+            ),
+            Chip(
+              label: Text("Deneme 1"),
+              avatar:
+                  CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
+            ),
+            Chip(
+              label: Text("Deneme 1"),
+              avatar:
+                  CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
+            ),
+            Chip(
+              label: Text("Deneme 1"),
+              avatar:
+                  CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
+            ),
+            Chip(
+              label: Text("Deneme 1"),
+              avatar:
+                  CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
+            ),
+            Chip(
+              label: Text("Deneme 1"),
+              avatar:
+                  CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
+            ),
+            Chip(
+              label: Text("Deneme 1"),
+              avatar:
+                  CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
+            ),
+            Chip(
+              label: Text("Deneme 1"),
+              avatar:
+                  CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
+            ),
+            Chip(
+              label: Text("Deneme 1"),
+              avatar:
+                  CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
+            ),
+            Chip(
+              label: Text("Deneme 1"),
+              avatar:
+                  CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
+            ),
+            Chip(
+              label: Text("Deneme 1"),
+              avatar:
+                  CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
+            ),
+            Chip(
+              label: Text("Deneme 1"),
+              avatar:
+                  CircleAvatar(backgroundColor: Colors.blue, child: Text("A")),
+            ),
+          ]),
     ));
   }
 }
