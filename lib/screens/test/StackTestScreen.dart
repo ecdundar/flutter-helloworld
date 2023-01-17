faad9d14707aed193e835ce0442fc185cd7f88f3
Eyüp Can Dündar <eyup@ecdyazilim.com>
2023-01-17 14:10:13 +0300

    Stack Screen RTL

--- lib/screens/test/StackTestScreen.dart
+++ lib/screens/test/StackTestScreen.dart
@@ -12,26 +12,23 @@ class _StackTestScreenState extends State<StackTestScreen> {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.yellow,
-      child: Stack(
-          alignment: AlignmentDirectional.center,
-          textDirection: TextDirection.rtl,
-          children: [
-            Container(
-              width: 200,
-              height: 200,
-              color: Colors.red,
-            ),
-            Container(
-              width: 180,
-              height: 180,
-              color: Colors.blue,
-            ),
-            Container(
-              width: 160,
-              height: 160,
-              color: Colors.green,
-            ),
-          ]),
+      child: Stack(alignment: AlignmentDirectional.center, children: [
+        Container(
+          width: 200,
+          height: 200,
+          color: Colors.red,
+        ),
+        Container(
+          width: 180,
+          height: 180,
+          color: Colors.blue,
+        ),
+        Container(
+          width: 160,
+          height: 160,
+          color: Colors.green,
+        ),
+      ]),
     );
   }
 }
