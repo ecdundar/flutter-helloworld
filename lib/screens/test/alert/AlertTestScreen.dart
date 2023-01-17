fb531654602c769c0ea715eb2a5f4ea58d206de4
Eyüp Can Dündar <eyup@ecdyazilim.com>
2023-01-17 16:05:49 +0300

    Yeni

---
+++ lib/screens/test/alert/AlertTestScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AlertTestScreen extends StatefulWidget {
+  const AlertTestScreen({super.key});
+
+  @override
+  State<AlertTestScreen> createState() => _AlertTestScreenState();
+}
+
+class _AlertTestScreenState extends State<AlertTestScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Container());
+  }
+}
