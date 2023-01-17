056d828e301fe256e86b3e26ee0c5292aa633957
Eyüp Can Dündar <eyup@ecdyazilim.com>
2023-01-17 15:22:40 +0300

    Fractional Sized Box

--- lib/screens/test/FractionalySizedBoxTestScreen.dart
+++ lib/screens/test/FractionalySizedBoxTestScreen.dart
@@ -15,10 +15,12 @@ class _FractionalySizedBoxTestScreenState
     return Container(
         color: Colors.yellow,
         child: FractionallySizedBox(
+            widthFactor: 0.5,
+            heightFactor: 0.5,
             child: Container(
-          color: Colors.red,
-          width: 50,
-          height: 50,
-        )));
+              color: Colors.red,
+              width: 50,
+              height: 50,
+            )));
   }
 }
