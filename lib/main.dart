29e445023bfa7c901c31bc8bea604af8343b0979
Eyüp Can Dündar <eyup@ecdyazilim.com>
2023-01-18 16:55:05 +0300

    Main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        initialRoute: "/Alert",
+        initialRoute: "/Elevated",
         builder: EasyLoading.init(),
         routes: {
           "/": (context) => LoginScreen(),
