3ab3a0638b3f46175370373f6677bf2dfaab9f64
Eyüp Can Dündar <eyup@ecdyazilim.com>
2023-01-18 16:53:33 +0300

    SingleChildScrollView

--- lib/screens/test/button/ElevatedButtonTestScreen.dart
+++ lib/screens/test/button/ElevatedButtonTestScreen.dart
@@ -14,76 +14,201 @@ class _ElevatedButtonTestScreenState extends State<ElevatedButtonTestScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Elevated Test')),
-      body: Padding(
-          padding: EdgeInsets.all(20),
-          child: Container(
-              color: Colors.yellow,
+      body: SingleChildScrollView(
+          child: Padding(
               padding: EdgeInsets.all(20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  ElevatedButton(
-                      onPressed: () {
-                        //Button 2 ve Button 3 ün tıklandığında gizlenip gösterilmesi için yapıldı.
-                        _isVisible = !_isVisible;
-                        setState(() {});
-                        print("Butona basıldı");
-                      },
-                      onLongPress: () {
-                        print("Butona uzun basıldı");
-                      },
-                      child: Text("Button 1")),
-                  SizedBox(height: 20),
-                  _isVisible == false
-                      ? Container(height: 50)
-                      : ElevatedButton.icon(
+              child: Container(
+                  color: Colors.yellow,
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      ElevatedButton(
                           onPressed: () {
-                            print('Butona tıklandı');
+                            //Button 2 ve Button 3 ün tıklandığında gizlenip gösterilmesi için yapıldı.
+                            _isVisible = !_isVisible;
+                            setState(() {});
+                            print("Butona basıldı");
                           },
-                          icon: Icon(Icons.account_box,
-                              color: Colors.yellow, size: 50),
-                          label: Text('Button 2')),
-                  SizedBox(height: 20),
-                  Visibility(
-                      visible: _isVisible,
-                      child: ElevatedButton(
+                          onLongPress: () {
+                            print("Butona uzun basıldı");
+                          },
+                          child: Text("Button 1")),
+                      SizedBox(height: 20),
+                      _isVisible == false
+                          ? Container(height: 50)
+                          : ElevatedButton.icon(
+                              onPressed: () {
+                                print('Butona tıklandı');
+                              },
+                              icon: Icon(Icons.account_box,
+                                  color: Colors.yellow, size: 50),
+                              label: Text('Button 2')),
+                      SizedBox(height: 20),
+                      Visibility(
+                          visible: _isVisible,
+                          child: ElevatedButton(
+                              onPressed: () {
+                                print('tıklandı');
+                              },
+                              style: ButtonStyle(
+                                  foregroundColor:
+                                      MaterialStateProperty.all(Colors.white),
+                                  backgroundColor:
+                                      MaterialStateProperty.resolveWith(
+                                          (states) {
+                                    //Butona tıklandığında yeşil renk yapması için eklendi
+                                    if (states
+                                        .contains(MaterialState.pressed)) {
+                                      return Colors.blueGrey;
+                                    }
+                                    return Colors.purple;
+                                  })),
+                              child: Text('Button 3'))),
+                      SizedBox(height: 20),
+                      ElevatedButton(
                           onPressed: () {
-                            print('tıklandı');
+                            print('Shadow button tıklandı');
                           },
                           style: ButtonStyle(
-                              foregroundColor:
-                                  MaterialStateProperty.all(Colors.white),
-                              backgroundColor:
-                                  MaterialStateProperty.resolveWith((states) {
-                                //Butona tıklandığında yeşil renk yapması için eklendi
-                                if (states.contains(MaterialState.pressed)) {
-                                  return Colors.blueGrey;
-                                }
-                                return Colors.purple;
-                              })),
-                          child: Text('Button 3'))),
-                  SizedBox(height: 20),
-                  ElevatedButton(
-                      onPressed: () {
-                        print('Shadow button tıklandı');
-                      },
-                      style: ButtonStyle(
-                          shadowColor: MaterialStateProperty.all(Colors.green),
-                          elevation: MaterialStateProperty.all(8)),
-                      child: Text('Shadow Elevated Button')),
-                  SizedBox(height: 20),
-                  ElevatedButton(
-                      onPressed: () {
-                        print('Shadow button tıklandı');
-                      },
-                      style: ButtonStyle(
-                          side: MaterialStateProperty.all(
-                              BorderSide(color: Colors.red, width: 2)),
-                          shadowColor: MaterialStateProperty.all(Colors.green),
-                          elevation: MaterialStateProperty.all(8)),
-                      child: Text('Bordered Button'))
-                ],
-              ))),
+                              shadowColor:
+                                  MaterialStateProperty.all(Colors.green),
+                              elevation: MaterialStateProperty.all(8)),
+                          child: Text('Shadow Elevated Button')),
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                          onPressed: () {
+                            print('Shadow button tıklandı');
+                          },
+                          style: ButtonStyle(
+                              side: MaterialStateProperty.all(
+                                  BorderSide(color: Colors.red, width: 2)),
+                              shadowColor:
+                                  MaterialStateProperty.all(Colors.green),
+                              elevation: MaterialStateProperty.all(8)),
+                          child: Text('Bordered Button')),
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                          onPressed: () {
+                            print('Shadow button tıklandı');
+                          },
+                          style: ButtonStyle(
+                              side: MaterialStateProperty.all(
+                                  BorderSide(color: Colors.red, width: 2)),
+                              shadowColor:
+                                  MaterialStateProperty.all(Colors.green),
+                              elevation: MaterialStateProperty.all(8)),
+                          child: Text('Bordered Button')),
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                          onPressed: () {
+                            print('Shadow button tıklandı');
+                          },
+                          style: ButtonStyle(
+                              side: MaterialStateProperty.all(
+                                  BorderSide(color: Colors.red, width: 2)),
+                              shadowColor:
+                                  MaterialStateProperty.all(Colors.green),
+                              elevation: MaterialStateProperty.all(8)),
+                          child: Text('Bordered Button')),
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                          onPressed: () {
+                            print('Shadow button tıklandı');
+                          },
+                          style: ButtonStyle(
+                              side: MaterialStateProperty.all(
+                                  BorderSide(color: Colors.red, width: 2)),
+                              shadowColor:
+                                  MaterialStateProperty.all(Colors.green),
+                              elevation: MaterialStateProperty.all(8)),
+                          child: Text('Bordered Button')),
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                          onPressed: () {
+                            print('Shadow button tıklandı');
+                          },
+                          style: ButtonStyle(
+                              side: MaterialStateProperty.all(
+                                  BorderSide(color: Colors.red, width: 2)),
+                              shadowColor:
+                                  MaterialStateProperty.all(Colors.green),
+                              elevation: MaterialStateProperty.all(8)),
+                          child: Text('Bordered Button')),
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                          onPressed: () {
+                            print('Shadow button tıklandı');
+                          },
+                          style: ButtonStyle(
+                              side: MaterialStateProperty.all(
+                                  BorderSide(color: Colors.red, width: 2)),
+                              shadowColor:
+                                  MaterialStateProperty.all(Colors.green),
+                              elevation: MaterialStateProperty.all(8)),
+                          child: Text('Bordered Button')),
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                          onPressed: () {
+                            print('Shadow button tıklandı');
+                          },
+                          style: ButtonStyle(
+                              side: MaterialStateProperty.all(
+                                  BorderSide(color: Colors.red, width: 2)),
+                              shadowColor:
+                                  MaterialStateProperty.all(Colors.green),
+                              elevation: MaterialStateProperty.all(8)),
+                          child: Text('Bordered Button')),
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                          onPressed: () {
+                            print('Shadow button tıklandı');
+                          },
+                          style: ButtonStyle(
+                              side: MaterialStateProperty.all(
+                                  BorderSide(color: Colors.red, width: 2)),
+                              shadowColor:
+                                  MaterialStateProperty.all(Colors.green),
+                              elevation: MaterialStateProperty.all(8)),
+                          child: Text('Bordered Button')),
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                          onPressed: () {
+                            print('Shadow button tıklandı');
+                          },
+                          style: ButtonStyle(
+                              side: MaterialStateProperty.all(
+                                  BorderSide(color: Colors.red, width: 2)),
+                              shadowColor:
+                                  MaterialStateProperty.all(Colors.green),
+                              elevation: MaterialStateProperty.all(8)),
+                          child: Text('Bordered Button')),
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                          onPressed: () {
+                            print('Shadow button tıklandı');
+                          },
+                          style: ButtonStyle(
+                              side: MaterialStateProperty.all(
+                                  BorderSide(color: Colors.red, width: 2)),
+                              shadowColor:
+                                  MaterialStateProperty.all(Colors.green),
+                              elevation: MaterialStateProperty.all(8)),
+                          child: Text('Bordered Button')),
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                          onPressed: () {
+                            print('Shadow button tıklandı');
+                          },
+                          style: ButtonStyle(
+                              side: MaterialStateProperty.all(
+                                  BorderSide(color: Colors.red, width: 2)),
+                              shadowColor:
+                                  MaterialStateProperty.all(Colors.green),
+                              elevation: MaterialStateProperty.all(8)),
+                          child: Text('Bordered Button'))
+                    ],
+                  )))),
     );
   }
 }
